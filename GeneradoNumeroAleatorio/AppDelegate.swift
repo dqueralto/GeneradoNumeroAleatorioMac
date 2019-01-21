6efdceadfcf70a7431348fe281a14a9a93551e4e
danielqparra <queraltoies@gmail.com>
2019-01-21 19:31:37 +0100

    Initial Commit

---
+++ GeneradoNumeroAleatorio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GeneradoNumeroAleatorio
+//
+//  Created by Daniel Queraltó Parra on 21/01/2019.
+//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GeneradoNumeroAleatorio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GeneradoNumeroAleatorio
+//
+//  Created by Daniel Queraltó Parra on 21/01/2019.
+//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
